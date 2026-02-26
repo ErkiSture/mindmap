1b627ae15a6fbdcd36aa17dd9fb3304c2e0a930c
ErkiSture <starmine04@gmail.com>
2026-02-26 12:23:44 +0100

    add form styling and can now create user with form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,12 +1,25 @@
 export default function Register() {
   return (
     <>
-    <h1>REGISTER PAGE</h1>
-      <form method="POST" action="api/auth/register">
-        <input type="text" />
-        <input type="password" />
-        <input type="submit" />
-      </form>
+    <h1>Register</h1>
+    <form method="POST" action="api/auth/register">
+      <div className="label-field">
+      <label htmlFor="username">Username</label>
+      <input type="text" name="username" placeholder="Enter username..."/>
+      </div>
+      <div className="label-field">
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" placeholder="Enter password..."/>
+      </div>
+      <div className="label-field">
+      <label htmlFor="confirm">Confirm password</label>
+      <input type="password" name="passwordConfirm" placeholder="Confirm password..."/>
+      </div>
+      <div className="register-form-actions">
+      <button type="submit">Register</button>
+      <a href="/login">Go to login page</a>
+      </div>
+    </form>
     </>
   )
 }
